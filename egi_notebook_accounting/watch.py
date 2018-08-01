d92ec9db066c4fce37de01203e984e89d807a7fc
Enol Fernandez <enol.fernandez@egi.eu>
2018-08-01 07:55:55 +0100

    Code reorg

--- egi_notebook_accounting/watch.py
+++ egi_notebook_accounting/watch.py
@@ -1,4 +1,5 @@
 import datetime
+import logging
 import json
 import os
 
@@ -6,63 +7,72 @@ from kubernetes import client, config, watch
 import requests
 
 
-def main(namespace='', prometheus_url='http://localhost:9000', spool_dir='spool'):
+def get_usage_info(prometheus_url, namespace, pod_name):
+    # TODO(enolfc): Making multiple queries here?
+    query_str = ("container_cpu_usage_seconds_total{{namespace='{0}',"
+                 "pod_name='{1}',container_name='notebook'}}")
+    params = {'query': query_str.format(namespace, pod_name)}
+    r = requests.get('{0}/api/v1/query'.format(prometheus_url), params=params)
+    result = r.json()['data']['result']
+    # take the last value
+    return result[-1]['value'][1]
+
+
+def process_event(event, namespace, prometheus_url, spool_dir):
+    pod = event['object']
+    username = pod.metadata.annotations.get('hub.jupyter.org/username',
+                                            'nobody')
+    logging.debug("Got %s for pod %s (user: %s)",
+                  event['type'],
+                  pod.medata.uid,
+                  username)
+
+    pod_file = os.path.join(spool_dir, pod.metadata.uid)
+    notebook = {'uid': pod.metadata.uid,
+                'user': username}
+    try:
+        if os.path.exists(pod_file):
+            with open(pod_file, 'r') as f:
+                notebook = json.loads(f.read())
+    except:
+        # do not care much, may be the first time we see the pod
+        pass
+
+    if pod.status.container_statuses:
+        state = pod.status.container_statuses[0].state
+        if state.running:
+            log.debug("Pod is running: %s", state.running)
+            if state.running.started_at:
+                notebook['start'] = state.running.started_at.isoformat()
+            else:
+                notebook['start'] = datetime.datetime.now().isoformat()
+        if state.terminated:
+            log.debug("Pod is terminated: %s", state.terminated)
+            if state.terminated.finished_at:
+                notebook['end'] = state.terminated.finished_at.isoformat()
+            else:
+                notebook['end'] = datetime.datetime.now().isoformat()
+            # Making multiple queries here
+            notebook['cpu_time'] = get_usage_info(prometheus_url,
+                                                  namespace,
+                                                  pod.metadata.name)
+    log.debug("Write pod_file %s with %s", pod_file, notebook)
+    with open(pod_file, 'w+') as f:
+        f.write(json.dumps(notebook))
+
+
+def watch(namespace='', prometheus_url='', spool_dir='spool'):
     config.load_incluster_config()
-
     v1 = client.CoreV1Api()
-
     w = watch.Watch()
-    for event in w.stream(v1.list_namespaced_pod, namespace=namespace, label_selector='component=singleuser-server'):
-        pod = event['object']
-        print("Event: %s %s %s" % (event['type'],event['object'].kind, event['object'].metadata.name))
-        #print("E2: %s %s %s" % (event['object'].metadata.uid,event['object'].kind, event['object'].metadata.name))
-        #print("Event: %s %s" % (event, dir(event)))
-        print("POD: %s" % (pod.metadata.uid))
-        print("ANN: %s" % (pod.metadata.annotations))
+    for event in w.stream(v1.list_namespaced_pod, namespace=namespace,
+                          label_selector='component=singleuser-server'):
+        process_event(event, namespace, prometheus_url, spool_dir)
 
-        pod_file = os.path.join(spool_dir, pod.metadata.uid)
-        notebook = {'uid': pod.metadata.uid,
-                    'user': pod.metadata.annotations.get('hub.jupyter.org/username')}
-        try:
-            if os.path.exists(pod_file):
-                with open(pod_file, 'r') as f:
-                    notebook = json.loads(f.read())
-        except Exception as e:
-            print(e)
-            print(pod_file)
 
-        if pod.status.container_statuses:
-            state = pod.status.container_statuses[0].state
-            print("STATE: %s" % pod.status.container_statuses)
-            if state.running:
-                print("RUN: %s" % pod.status.container_statuses[0].state.running.started_at)
-                if state.running.started_at:
-                    notebook['start'] = state.running.started_at.isoformat()
-                else:
-                    notebook['start'] = datetime.datetime.now().isoformat()
-            if state.terminated:
-                # Gone, if we dont have a terminated at, we craft one
-                print("DEL: %s" % pod.status.container_statuses[0].state.terminated.finished_at)
-                if state.terminated.finished_at:
-                    notebook['end'] = state.terminated.finished_at.isoformat()
-                else:
-                    notebook['end'] = datetime.datetime.now().isoformat()
-                # Making multiple queries here
-                query = ("container_cpu_usage_seconds_total{{namespace='{0}',"
-                         "pod_name='{1}',container_name='notebook'}}").format(namespace, pod.metadata.name)
-                r = requests.get('{0}/api/v1/query'.format(prometheus_url),
-                                     params={'query': query})
-                try:
-                    print("HERE!")
-                    print(r.json()['data']['result'])
-                    print(r.json()['data']['result'][-1]['value'][1])
-                    notebook['cpu_time'] = r.json()['data']['result'][-1]['value'][1]
-                except:
-                    pass
-        print("NOTEBOOK: %s" % notebook)
-        with open(pod_file, 'w+') as f:
-            f.write(json.dumps(notebook))
+def main():
+    watch('training', 'http://prom-prometheus-server.default.svc.cluster.local')
 
 
 if __name__ == '__main__':
-    main('training', 'http://prom-prometheus-server.default.svc.cluster.local')
+    main()
