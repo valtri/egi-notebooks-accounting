8a73dc7ff37bd4c7f948dc15ffd25e25e4cbde53
Enol Fernandez <enol.fernandez@egi.eu>
2018-08-08 07:40:53 +0100

    Move to a sqlite DB with peewee

--- egi_notebooks_accounting/watch.py
+++ egi_notebooks_accounting/watch.py
@@ -4,21 +4,36 @@ import json
 import os
 
 from kubernetes import client, config, watch
+from peewee import BooleanField, CharField, DateTimeField, DoubleField, \
+    SqliteDatabase, Model
 import requests
-
 
 # Some constants, may be moved to configuration
 USERNAME_ANNOTATION = 'hub.jupyter.org/username'
 DEFAULT_USERNAME = 'nobody'
 DEFAULT_SPOOL_DIR = '/var/spool/egi-notebooks/'
-DEFAULT_NS = 'default'
+DEFAULT_NAMESPACE = 'default'
 DEFAULT_PROMETHEUS_URL = 'http://localhost:9000'
+DEFAULT_NOTEBOOK_DB = 'notebooks.db'
 
 # name of the environment variables where config is expected
 NAMESPACE_ENV = 'NOTEBOOKS_NS'
 SPOOL_DIR_ENV = 'SPOOL_DIR'
 PROMETHEUS_URL_ENV = 'PROMETHEUS_URL'
+NOTEBOOK_DB_ENV = 'NOTEBOOK_DB'
 
+db = SqliteDatabase(None)
+
+class Notebook(Model):
+    uid = CharField()
+    username = CharField()
+    start = DateTimeField(null=True)
+    end = DateTimeField(null=True)
+    cpu_time = DoubleField(null=True)
+    processed = BooleanField(default=False)
+
+    class Meta:
+        database = db
 
 def get_usage_info(prometheus_url, namespace, pod_name):
     # TODO(enolfc): Making multiple queries here?
@@ -40,38 +55,30 @@ def process_event(event, namespace, prometheus_url, spool_dir):
                   pod.medata.uid,
                   username)
 
-    pod_file = os.path.join(spool_dir, pod.metadata.uid)
-    notebook = {'uid': pod.metadata.uid,
-                'user': username}
     try:
-        if os.path.exists(pod_file):
-            with open(pod_file, 'r') as f:
-                notebook = json.loads(f.read())
-    except:
-        # do not care much, may be the first time we see the pod
-        pass
+        notebook = Notebook.get(Notebook.uid == pod.metadata.uid)
+    except Notebook.DoesNotExist:
+        notebook = Notebook.get(uid=pod.metadata.uid, username=username)
 
     if pod.status.container_statuses:
         state = pod.status.container_statuses[0].state
         if state.running:
             log.debug("Pod is running: %s", state.running)
             if state.running.started_at:
-                notebook['start'] = state.running.started_at.isoformat()
+                notebook.start = state.running.started_at
             else:
-                notebook['start'] = datetime.datetime.now().isoformat()
+                notebook.start = datetime.datetime.now()
         if state.terminated:
             log.debug("Pod is terminated: %s", state.terminated)
             if state.terminated.finished_at:
-                notebook['end'] = state.terminated.finished_at.isoformat()
+                notebook.end = state.terminated.finished_at
             else:
-                notebook['end'] = datetime.datetime.now().isoformat()
-            # Making multiple queries here
-            notebook['cpu_time'] = get_usage_info(prometheus_url,
-                                                  namespace,
-                                                  pod.metadata.name)
-    log.debug("Write pod_file %s with %s", pod_file, notebook)
-    with open(pod_file, 'w+') as f:
-        f.write(json.dumps(notebook))
+                notebook.end = datetime.datetime.now()
+            # TODO(enolfc): Make multiple queries here
+            notebook.cpu_time = get_usage_info(prometheus_url,
+                                               namespace,
+                                               pod.metadata.name)
+    notebook.save()
 
 
 def watch(namespace='', prometheus_url='', spool_dir=''):
@@ -84,9 +91,12 @@ def watch(namespace='', prometheus_url='', spool_dir=''):
 
 
 def main():
-    namespace = os.environt.get(NAMESAPACE_ENV, DEFAULT_NAMESPACE)
+    namespace = os.environ.get(NAMESPACE_ENV, DEFAULT_NAMESPACE)
     spool_dir = os.environ.get(SPOOL_DIR_ENV, DEFAULT_SPOOL_DIR)
     prometheus_url = os.environ.get(PROMETHEUS_URL_ENV, DEFAULT_PROMETHEUS_URL)
+    db.init(os.environ.get(NOTEBOOK_DB_ENV, DEFAULT_NOTEBOOK_DB))
+    db.connect()
+    db.create_tables([Notebook])
     watch(namespace, prometheus_url, spool_dir)
 
 
