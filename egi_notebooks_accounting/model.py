1697c559c0be51c22bb7db48d195614d460b34e8
Enol Fernandez <enol.fernandez@egi.eu>
2018-08-08 10:42:21 +0100

    Moved model to different module

---
+++ egi_notebooks_accounting/model.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+import peewee
+
+# default file for the notebooks database
+DEFAULT_NOTEBOOK_DB = 'notebooks.sqlite'
+# environment variable to get the notebooks database file
+NOTEBOOK_DB_ENV = 'NOTEBOOK_DB'
+
+db = peewee.SqliteDatabase(None)
+
+class Notebook(peewee.Model):
+    uid = peewee.CharField()
+    username = peewee.CharField()
+    start = peewee.FloatField(null=True)
+    end = peewee.FloatField(null=True)
+    cpu_time = peewee.DoubleField(null=True)
+    processed = peewee.BooleanField(default=False)
+
+    class Meta:
+        database = db
+
+
+def init_db(db_file=''):
+    if not db_file:
+        db_file = os.environ.get(NOTEBOOK_DB_ENV, DEFAULT_NOTEBOOK_DB)
+    logging.debug('Creating DB at %s', db_file)
+    db.init(db_file)
+    db.connect()
+    db.create_tables([Notebook])
+    db.close()
+    return db
--- egi_notebooks_accounting/watch.py
+++ egi_notebooks_accounting/watch.py
@@ -7,30 +7,16 @@ import kubernetes
 import peewee
 import requests
 
-# Some constants, may be moved to configuration
+from .model import db, init_db, Notebook
+
+# Some constants, may be moved somewhere else
 USERNAME_ANNOTATION = 'hub.jupyter.org/username'
 DEFAULT_USERNAME = 'nobody'
 DEFAULT_NAMESPACE = 'default'
 DEFAULT_PROMETHEUS_URL = 'http://localhost:9000'
-DEFAULT_NOTEBOOK_DB = 'notebooks.db'
-
 # name of the environment variables where config is expected
 NAMESPACE_ENV = 'NOTEBOOKS_NS'
 PROMETHEUS_URL_ENV = 'PROMETHEUS_URL'
-NOTEBOOK_DB_ENV = 'NOTEBOOK_DB'
-
-db = peewee.SqliteDatabase(None)
-
-class Notebook(peewee.Model):
-    uid = peewee.CharField()
-    username = peewee.CharField()
-    start = peewee.DateTimeField(null=True)
-    end = peewee.DateTimeField(null=True)
-    cpu_time = peewee.DoubleField(null=True)
-    processed = peewee.BooleanField(default=False)
-
-    class Meta:
-        database = db
 
 def get_usage_info(prometheus_url, namespace, pod_name):
     # TODO(enolfc): Making multiple queries here?
@@ -57,6 +43,7 @@ def process_event(event, namespace, prometheus_url):
                   pod.metadata.uid,
                   username)
 
+    db.connect()
     try:
         notebook = Notebook.get(Notebook.uid == pod.metadata.uid)
     except Notebook.DoesNotExist:
@@ -64,27 +51,28 @@ def process_event(event, namespace, prometheus_url):
 
     if not notebook.start:
         # first time we see the pod, use current time as start
-        notebook.start = datetime.datetime.now()
+        notebook.start = datetime.datetime.now().timestamp()
 
     if event['type'] == 'DELETED' and not notebook.end:
         # make sure we capture some end time
-        notebook.end = datetime.datetime.now()
+        notebook.end = datetime.datetime.now().timestamp()
 
     if pod.status.container_statuses:
         state = pod.status.container_statuses[0].state
         logging.debug("Pod state: %s" % state)
         if state.running and state.running.started_at:
             logging.debug("Got start date from k8s: %s", state.running.started_at)
-            notebook.start = state.running.started_at
+            notebook.start = state.running.started_at.timestamp()
         if state.terminated and state.terminated.finished_at:
             logging.debug("Got terminated date from k8s: %s", state.terminated.finished_at)
-            notebook.end = state.terminated.finished_at
+            notebook.end = state.terminated.finished_at.timestamp()
 
     if notebook.end:
         notebook.cpu_time = get_usage_info(prometheus_url,
                                            namespace,
                                            pod.metadata.name)
     notebook.save()
+    db.close()
 
 
 def watch(namespace='', prometheus_url=''):
@@ -98,15 +86,11 @@ def watch(namespace='', prometheus_url=''):
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
+    init_db()
     namespace = os.environ.get(NAMESPACE_ENV, DEFAULT_NAMESPACE)
     logging.debug('Namespace to watch: %s', namespace)
     prometheus_url = os.environ.get(PROMETHEUS_URL_ENV, DEFAULT_PROMETHEUS_URL)
     logging.debug('Prometheus server at %s', prometheus_url)
-    db_file = os.environ.get(NOTEBOOK_DB_ENV, DEFAULT_NOTEBOOK_DB)
-    logging.debug('Creating DB at %s', db_file)
-    db.init(db_file)
-    db.connect()
-    db.create_tables([Notebook])
     watch(namespace, prometheus_url)
 
 
