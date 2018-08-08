7c61a5fb911f1f25c11903f60db4f7460b767284
Enol Fernandez <enol.fernandez@egi.eu>
2018-08-08 08:39:00 +0100

    Fixes after testing for real

--- egi_notebooks_accounting/watch.py
+++ egi_notebooks_accounting/watch.py
@@ -3,34 +3,31 @@ import logging
 import json
 import os
 
-from kubernetes import client, config, watch
-from peewee import BooleanField, CharField, DateTimeField, DoubleField, \
-    SqliteDatabase, Model
+import kubernetes
+import peewee
 import requests
 
 # Some constants, may be moved to configuration
 USERNAME_ANNOTATION = 'hub.jupyter.org/username'
 DEFAULT_USERNAME = 'nobody'
-DEFAULT_SPOOL_DIR = '/var/spool/egi-notebooks/'
 DEFAULT_NAMESPACE = 'default'
 DEFAULT_PROMETHEUS_URL = 'http://localhost:9000'
 DEFAULT_NOTEBOOK_DB = 'notebooks.db'
 
 # name of the environment variables where config is expected
 NAMESPACE_ENV = 'NOTEBOOKS_NS'
-SPOOL_DIR_ENV = 'SPOOL_DIR'
 PROMETHEUS_URL_ENV = 'PROMETHEUS_URL'
 NOTEBOOK_DB_ENV = 'NOTEBOOK_DB'
 
-db = SqliteDatabase(None)
+db = peewee.SqliteDatabase(None)
 
-class Notebook(Model):
-    uid = CharField()
-    username = CharField()
-    start = DateTimeField(null=True)
-    end = DateTimeField(null=True)
-    cpu_time = DoubleField(null=True)
-    processed = BooleanField(default=False)
+class Notebook(peewee.Model):
+    uid = peewee.CharField()
+    username = peewee.CharField()
+    start = peewee.DateTimeField(null=True)
+    end = peewee.DateTimeField(null=True)
+    cpu_time = peewee.DoubleField(null=True)
+    processed = peewee.BooleanField(default=False)
 
     class Meta:
         database = db
@@ -43,61 +40,74 @@ def get_usage_info(prometheus_url, namespace, pod_name):
     r = requests.get('{0}/api/v1/query'.format(prometheus_url), params=params)
     result = r.json()['data']['result']
     # take the last value
-    return result[-1]['value'][1]
+    try:
+        return result[-1]['value'][1]
+    except IndexError:
+        # oops, what happened?
+        logging.error('Unexpected exception while processing: %s', result)
+        return 0.
 
 
-def process_event(event, namespace, prometheus_url, spool_dir):
+def process_event(event, namespace, prometheus_url):
     pod = event['object']
     username = pod.metadata.annotations.get(USERNAME_ANNOTATION,
                                             DEFAULT_USERNAME)
     logging.debug("Got %s for pod %s (user: %s)",
                   event['type'],
-                  pod.medata.uid,
+                  pod.metadata.uid,
                   username)
 
     try:
         notebook = Notebook.get(Notebook.uid == pod.metadata.uid)
     except Notebook.DoesNotExist:
-        notebook = Notebook.get(uid=pod.metadata.uid, username=username)
+        notebook = Notebook(uid=pod.metadata.uid, username=username)
+
+    if not notebook.start:
+        # first time we see the pod, use current time as start
+        notebook.start = datetime.datetime.now()
+
+    if event['type'] == 'DELETED' and not notebook.end:
+        # make sure we capture some end time
+        notebook.end = datetime.datetime.now()
 
     if pod.status.container_statuses:
         state = pod.status.container_statuses[0].state
-        if state.running:
-            log.debug("Pod is running: %s", state.running)
-            if state.running.started_at:
-                notebook.start = state.running.started_at
-            else:
-                notebook.start = datetime.datetime.now()
-        if state.terminated:
-            log.debug("Pod is terminated: %s", state.terminated)
-            if state.terminated.finished_at:
-                notebook.end = state.terminated.finished_at
-            else:
-                notebook.end = datetime.datetime.now()
-            # TODO(enolfc): Make multiple queries here
-            notebook.cpu_time = get_usage_info(prometheus_url,
-                                               namespace,
-                                               pod.metadata.name)
+        logging.debug("Pod state: %s" % state)
+        if state.running and state.running.started_at:
+            logging.debug("Got start date from k8s: %s", state.running.started_at)
+            notebook.start = state.running.started_at
+        if state.terminated and state.terminated.finished_at:
+            logging.debug("Got terminated date from k8s: %s", state.terminated.finished_at)
+            notebook.end = state.terminated.finished_at
+
+    if notebook.end:
+        notebook.cpu_time = get_usage_info(prometheus_url,
+                                           namespace,
+                                           pod.metadata.name)
     notebook.save()
 
 
-def watch(namespace='', prometheus_url='', spool_dir=''):
-    config.load_incluster_config()
-    v1 = client.CoreV1Api()
-    w = watch.Watch()
+def watch(namespace='', prometheus_url=''):
+    kubernetes.config.load_incluster_config()
+    v1 = kubernetes.client.CoreV1Api()
+    w = kubernetes.watch.Watch()
     for event in w.stream(v1.list_namespaced_pod, namespace=namespace,
                           label_selector='component=singleuser-server'):
-        process_event(event, namespace, prometheus_url, spool_dir)
+        process_event(event, namespace, prometheus_url)
 
 
 def main():
+    logging.basicConfig(level=logging.DEBUG)
     namespace = os.environ.get(NAMESPACE_ENV, DEFAULT_NAMESPACE)
-    spool_dir = os.environ.get(SPOOL_DIR_ENV, DEFAULT_SPOOL_DIR)
+    logging.debug('Namespace to watch: %s', namespace)
     prometheus_url = os.environ.get(PROMETHEUS_URL_ENV, DEFAULT_PROMETHEUS_URL)
-    db.init(os.environ.get(NOTEBOOK_DB_ENV, DEFAULT_NOTEBOOK_DB))
+    logging.debug('Prometheus server at %s', prometheus_url)
+    db_file = os.environ.get(NOTEBOOK_DB_ENV, DEFAULT_NOTEBOOK_DB)
+    logging.debug('Creating DB at %s', db_file)
+    db.init(db_file)
     db.connect()
     db.create_tables([Notebook])
-    watch(namespace, prometheus_url, spool_dir)
+    watch(namespace, prometheus_url)
 
 
 if __name__ == '__main__':
