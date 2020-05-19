f2dfda199a8909688e467e9dcea3eb0a37efaef7
Enol Fernández <enol.fernandez@egi.eu>
2020-05-19 12:07:35 +0100

    Deploy (#7) (#8)
    * Try to fix deployment errors
    
    * Fix parameter
    
    * Reset changes before building

--- build.sh
+++ build.sh
@@ -7,12 +7,7 @@ if [ "x$TRAVIS_TAG" != "x" ]; then
      CHARTPRESS_OPT="$CHARTPRESS_OPT --tag $TRAVIS_TAG"
 fi
 
-if [ "x$TRAVIS_COMMIT_RANGE" != "x" ]; then
-    CHARTPRESS_OPT="$CHARTPRESS_OPT --commit-range $TRAVIS_COMMIT_RANGE"
-fi
-
-
-if [ "x$1" == "x--deploy" ]; then
+if [ "x$DOCKER_PASSWORD" != "x" ]; then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     CHARTPRESS_OPT="$CHARTPRESS_OPT --push --publish-chart"
 fi
