e96cbc0e05006943c0b94af1ad10e6b20a149ef7
Enol Fernandez <enol.fernandez@egi.eu>
2024-07-23 17:49:00 +0100

    Add timestamp file for reporting
    The code will report from the last succesfully completed execution

--- egi_notebooks_accounting/eosc.py
+++ egi_notebooks_accounting/eosc.py
@@ -37,6 +37,7 @@ client_secret=<client secret>
 client_id=<client_id>
 accounting_url=https://api.acc.staging.eosc.grnet.gr
 installaion_id=<id of the installation to report accounting for>
+timestamp_file=<file where the timestamp of the last run is kept>
 
 [eosc.flavors]
 # contains a list of flavors and metrics they are mapped to
@@ -53,6 +54,7 @@ from configparser import ConfigParser
 from datetime import date, datetime, timedelta
 
 import dateutil.parser
+import pytz
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -64,6 +66,7 @@ FLAVOR_CONFIG = "eosc.flavors"
 DEFAULT_CONFIG_FILE = "config.ini"
 DEFAULT_TOKEN_URL = "https://proxy.staging.eosc-federation.eu/OIDC/token"
 DEFAULT_ACCOUNTING_URL = "https://api.acc.staging.eosc.grnet.gr"
+DEFAULT_TIMESTAMP_FILE = "eosc-accounting.timestamp"
 
 
 def get_access_token(token_url, client_id, client_secret):
@@ -103,6 +106,80 @@ def update_pod_metric(pod, metrics, flavor_config):
     metrics[(user, group)] = user_metrics
 
 
+def get_from_to_dates(args, timestamp_file):
+    from_date = None
+    if args.from_date:
+        from_date = dateutil.parser.parse(args.from_date)
+    else:
+        try:
+            with open(timestamp_file, "r") as tsf:
+                try:
+                    from_date = dateutil.parser.parse(tsf.read())
+                    from_date += timedelta(minutes=1)
+                except dateutil.parser.ParserError as e:
+                    logging.debug(
+                        f"Invalid timestamp content in '{timestamp_file}': {e}"
+                    )
+        except OSError as e:
+            logging.debug(f"Not able to open timestamp file '{timestamp_file}': {e}")
+        # no date specified report from yesterday
+        if not from_date:
+            report_day = date.today() - timedelta(days=1)
+            from_date = datetime(
+                report_day.year, report_day.month, report_day.day, 0, 0
+            )
+    if args.to_date:
+        to_date = dateutil.parser.parse(args.to_date)
+    else:
+        # go until last minute of yesterday
+        report_day = date.today() - timedelta(days=1)
+        to_date = datetime(report_day.year, report_day.month, report_day.day, 23, 59)
+    utc = pytz.UTC
+    from_date = from_date.replace(tzinfo=utc)
+    to_date = to_date.replace(tzinfo=utc)
+    return from_date, to_date
+
+
+def generate_day_metrics(
+    period_start, period_end, accounting_url, token, flavor_config, dry_run
+):
+    logging.info(f"Generate metrics from {period_start} to {period_end}")
+    metrics = {}
+    # pods ending in between the reporting times
+    for pod in VM.select().where(
+        (VM.end_time >= period_start) & (VM.end_time <= period_end)
+    ):
+        update_pod_metric(pod, metrics, flavor_config)
+    # pods starting but not finished between the reporting times
+    for pod in VM.select().where(
+        (VM.start_time >= period_start) & (VM.end_time is None)
+    ):
+        update_pod_metric(pod, metrics, flavor_config)
+    period_start_str = period_start.strftime("%Y-%m-%dT%H:%M:%SZ")
+    period_end_str = period_end.strftime("%Y-%m-%dT%H:%M:%SZ")
+    for (user, group), flavors in metrics.items():
+        for metric_key, value in flavors.items():
+            metric_data = {
+                "metric_definition_id": metric_key,
+                "time_period_start": period_start_str,
+                "time_period_end": period_end_str,
+                "user": user,
+                "group": group,
+                "value": value,
+            }
+            logging.debug(f"Sending metric {metric_data} to accounting")
+            if dry_run:
+                logging.debug("Dry run, not sending")
+            else:
+                push_metric(accounting_url, token, installation, metric_data)
+    if not dry_run:
+        try:
+            with open(timestamp_file, "w+") as tsf:
+                tsf.write(period_end.strftime("%Y-%m-%dT%H:%M:%SZ"))
+        except OSError as e:
+            logging.debug("Failed to write timestamp file '{timestamp_file}': {e}")
+
+
 def main():
     parser = argparse.ArgumentParser(description="EOSC Accounting metric pusher")
     parser.add_argument(
@@ -136,53 +213,32 @@ def main():
     client_secret = os.environ.get(
         "CLIENT_SECRET", eosc_config.get("client_secret", "")
     )
+    if args.dry_run:
+        logging.debug("Not getting credentials, dry-run")
+        token = None
+    else:
+        token = get_access_token(token_url, client_id, client_secret)
+
     accounting_url = os.environ.get(
         "ACCOUNTING_URL", eosc_config.get("accounting_url", DEFAULT_ACCOUNTING_URL)
     )
     installation = eosc_config.get("installation_id", "")
 
-    # ==== queries ====
-    # TODO: keep the last reported day as state, do report from there
-    if args.from_date:
-        from_date = dateutil.parser.parse(args.from_date)
-    else:
-        report_day = date.today() - timedelta(days=1)
-        from_date = datetime(report_day.year, report_day.month, report_day.day, 0, 0)
-    if args.to_date:
-        to_date = dateutil.parser.parse(args.to_date)
-    else:
-        to_date = from_date + timedelta(hours=23, minutes=59)
-    logging.debug(f"Reporting from {from_date} to {to_date}")
-    metrics = {}
-    # pods ending in between the reporting times
-    for pod in VM.select().where((VM.end_time >= from_date) & (VM.end_time <= to_date)):
-        update_pod_metric(pod, metrics, flavor_config)
-    # pods starting but not finished between the reporting times
-    for pod in VM.select().where((VM.start_time >= from_date) & (VM.end_time is None)):
-        update_pod_metric(pod, metrics, flavor_config)
+    timestamp_file = os.environ.get(
+        "TIMESTAMP_FILE", eosc_config.get("timestamp_file", DEFAULT_TIMESTAMP_FILE)
+    )
 
-    # ==== push this to EOSC accounting ====
-    if args.dry_run:
-        logging.debug("Not getting credentials, dry-run")
-    else:
-        token = get_access_token(token_url, client_id, client_secret)
-    period_start = from_date.strftime("%Y-%m-%dT%H:%M:%SZ")
-    period_end = to_date.strftime("%Y-%m-%dT%H:%M:%SZ")
-    for (user, group), flavors in metrics.items():
-        for metric_key, value in flavors.items():
-            metric_data = {
-                "metric_definition_id": metric_key,
-                "time_period_start": period_start,
-                "time_period_end": period_end,
-                "user": user,
-                "group": group,
-                "value": value,
-            }
-            logging.debug(f"Sending metric {metric_data} to accounting")
-            if args.dry_run:
-                logging.debug("Dry run, not sending")
-            else:
-                push_metric(accounting_url, token, installation, metric_data)
+    # ==== queries ====
+    from_date, to_date = get_from_to_dates(args, timestamp_file)
+    logging.debug(f"Reporting from {from_date} to {to_date}")
+    # repeat in 24 hour intervals
+    period_start = from_date
+    while period_start <= to_date:
+        period_end = period_start + timedelta(hours=23, minutes=59)
+        generate_day_metrics(
+            period_start, period_end, accounting_url, token, flavor_config, args.dry_run
+        )
+        period_start = period_end + timedelta(minutes=1)
 
 
 if __name__ == "__main__":
